a75ed8196225ed2f7a6943838c8847e4e8f662a1
wangweijia08025 <wangweijia08025@hellobike.com>
2020-06-29 18:48:45 +0800

    快速排序

--- quickSort.js
+++ quickSort.js
@@ -3,71 +3,78 @@
 class QuickSort {
   // 交换
   static swap(arr, i, j) {
-    console.log('swap(arr, i, j)', arr, i, j);
     const temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
+    return arr;
   }
 
   // 返回等于部分的第一个元素的下标和最后一个下标组成的整数数组
   static partition(arr, l, r) {
-    console.log('-----3', arr, l, r);
 
     let left = l;
-    let basic = arr[r];
-    let less = l - 1;
-    let more = r + 1;
+    let right = r - 1;
 
-    while(left < more) {
-      if (arr[left] < basic) {
-        less += 1;
-        this.swap(arr, less, left)
+    const base = arr[r];
+
+    while(left < right) {
+      let endLeft = false;
+      let endRight = false;
+
+      if (arr[left] < base) {
         left += 1;
-      } else if (arr[left] > basic) {
-        more -= 1;
-        this.swap(arr, more, left);
       } else {
-        left += 1;
+        endLeft = left < right;
+      }
+
+      if (arr[right] > base) {
+        right -= 1; 
+      } else {
+        endRight = left < right;
+      }
+
+      if (endLeft && endRight) {
+        this.swap(arr, left, right);
+        endLeft = false;
+        endRight = false;
       }
     }
 
-    return [less + 1, more - 1];
-    // const list = [];
-    // for (let index = less + 1; index <= more - 1; index++) {
-    //   list.push(index);
-    // }
-
-    // return list;
+    if (arr[left] < base) {
+      this.swap(arr, left + 1, r); 
+    } else {
+      this.swap(arr, left, r); 
+    }
+    return left;
   }
 
   // 入口
-  static quickSort(arr) {
+  static main(arr) {
     if (arr === null || arr === undefined || arr.length < 2) {
       return
     }
     
-    this.quickSort2(arr, 0, arr.length - 1);
+    this.quickSort(arr, 0, arr.length - 1);
   }
 
 
-  static quickSort2(arr, l, r) {
-    console.log('-----2', arr, l, r);
+  static quickSort(arr, l, r) {
     if (l < r) {
+      console.log(arr, l, r);
+
       const randomN = parseInt(Math.random() * (r - l));
-      console.log('randomN:', randomN);
-      this.swap(arr, randomN + l, r);
+      this.swap(arr, randomN + l, r);      
+      let p = this.partition(arr, l, r);
 
-      console.log('arr::::', arr);
-
-      const p = this.partition(arr, l, r);
-      this.quickSort2(arr. l, p[0]-1);
-      this.quickSort2(arr. l, p[1] + 1, r);
+      console.log('------------------');
+      this.quickSort(arr, l, p);
+      this.quickSort(arr, p + 1, r);
     }
   }
 }
 
 console.log('-----1');
-const list = [2,6,3,8,4,6,1,5,2,9,4,7,5];
-QuickSort.quickSort(list);
+const list = [2,6,3,8,4,16,1,25,32,9,14,7,5];
+QuickSort.main(list);
 console.log('-----end');
 console.log(list);
